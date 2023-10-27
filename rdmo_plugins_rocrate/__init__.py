be8812905bbec64526f8d9fe9d7a482be6a1df88
David Wallace <david.wallace@tu-darmstadt.de>
2023-10-27 09:22:19 +0200

    build: mv setup to pyproject.toml

--- rdmo_plugins_rocrate/__init__.py
+++
@@ -1,8 +0,0 @@
-__title__ = 'rdmo-plugins'
-__version__ = '1.0.0'
-__author__ = 'Jochen Klar'
-__email__ = 'mail@jochenklar.de'
-__license__ = 'Apache-2.0'
-__copyright__ = 'Copyright 2020 RDMO-Arbeitsgemeinschaft'
-
-VERSION = __version__
